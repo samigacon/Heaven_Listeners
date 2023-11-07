ac2a3e3f355d3d68d5c28d448ef6c45353db7a83
sami-gacon <sami.gacon@gmail.com>
2023-11-07 15:32:10 +0100

    Customizing Front Project from template React + Vite

---
+++ client/src/App.js
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+function App() {
+  //states
+  const [state, setState] = useState(0)
+
+  //components
+
+
+  //render
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
