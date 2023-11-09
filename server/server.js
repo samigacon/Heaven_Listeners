94648f86ab4b3139a21746697648e0360fa3ae5e
samigacon <sami.gacon@gmail.com>
2023-11-09 20:18:12 +0000

    3WA IDE Compatibility

---
+++ server/server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const PORT = 3001;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`Server is launching with port ${PORT}`);
+});
