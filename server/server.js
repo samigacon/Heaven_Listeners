822a7d24d56dcf9f3dcd968927e86f25a1430001
samigacon <sami.gacon@gmail.com>
2023-11-15 14:06:29 +0000

    Setup Complete

--- server/server.js
+++ server/server.js
@@ -5,5 +5,5 @@ const PORT = 3001;
 app.use(express.json());
 
 app.listen(PORT, () => {
-  console.log(`Server is launching with port ${PORT}`);
+    console.log(`Server is launching with port ${PORT}`);
 });
