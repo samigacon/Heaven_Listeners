b81c2cd1e9b5f337cafdabddd0f9fa332fa689eb
samigacon <sami.gacon@gmail.com>
2023-11-16 11:38:05 +0000

    Creating Main Page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 export default function Footer() {
     return (
-        <footer className="footer">
+        <footer>
             <p>© 2023 Heaven Listeners - All rights reserved</p>
         </footer>
     )
--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,12 +1,20 @@
 import Loader from '../components/Loader'
 
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
 export default function HomePage () {
     
     
     
     return (
-        <>
-            <h2>Here is the Home Page !</h2>
-        </>
+        <div className="homepage">
+            <h1>Home</h1>
+            <a><h2 className="button-historic"><Link to="/historic">Historic</Link></h2></a>
+            <div className="home-historic"></div>
+            <a><h2 className="button-favorites"><Link to="/favorites">Favorites</Link></h2></a>
+            <div className="home-favorites"></div>
+            <a><h2 className="button-playlists"><Link to="/playlist">Playlists</Link></h2></a>
+            <div className="home-playlists"></div>
+        </div>
     )
 }
