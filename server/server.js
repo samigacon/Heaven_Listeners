3353bd07e8889158ae2aab4e524268c6c4eaf03b
samigacon <sami.gacon@gmail.com>
2023-11-17 14:54:20 +0000

    Establishing Creating Session, Login and Logout

--- server/server.js
+++ server/server.js
@@ -11,6 +11,10 @@ const PORT = 3001;
 
 const app = express();
 
+app.use(express.json())
+app.use(express.static('public'))
+app.use('/', routes.router)
+
 // Create Session
 app.use(session({
     store: new FileStore({
@@ -21,9 +25,35 @@ app.use(session({
     saveUninitialized: true
 }));
 
-app.use(express.json())
-app.use(express.static('public'))
-app.use('/', routes.router)
+// User State
+app.get('/', (req, res) => {
+  res.send(`User : ${req.session.connected ? 'Connected' : 'Disconnected'}`);
+});
+
+
+//Connection
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+  if (username === 'votre_nom_utilisateur' && password === 'votre_mot_de_passe') {
+    req.session.connected = true;
+    res.status(200).json({ message: 'Authentication Successful' });
+  } else {
+    res.status(401).json({ message: 'Authentication Failed' });
+  }
+});
+
+
+//Disconnection
+app.post('/logout', (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      console.error('Disconnection Error:', err);
+      res.status(500).json({ message: 'Error Logging Out' });
+    } else {
+      res.status(200).json({ message: 'Logged Out Successfully' });
+    }
+  });
+});
 
 
 app.listen(PORT, () => {
