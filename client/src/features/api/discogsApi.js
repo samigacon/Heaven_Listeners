2f9e573bd87c67e07c4122d68a673abac2962382
samigacon <sami.gacon@gmail.com>
2023-11-21 17:30:04 +0000

    discogsApi updated

--- client/src/features/api/discogsApi.js
+++ client/src/features/api/discogsApi.js
@@ -2,10 +2,10 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const discogsApi = createApi({
     reducerPath: 'discogsApi',
-    baseQuery: fetchBaseQuery({ baseUrl: "https://api.discogs.com/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3001/" }),
     endpoints: (builder) => ({
         searchTracks: builder.query({
-            query: (searchQuery) => `database/search?q=${searchQuery}&type=track`,
+            query: (searchQuery) => `database/search?q=${searchQuery}`,
         }),
     }),
 });
