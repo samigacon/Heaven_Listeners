86fc6604d7794abc302ec58b2a4aee9e010931be
samigacon <sami.gacon@gmail.com>
2023-11-26 14:17:23 +0000

    Optimize Useful Dependencies

--- server/server.js
+++ server/server.js
@@ -7,10 +7,6 @@ const FileStore = require('session-file-store')(session);
 const crypto = require('crypto');
 const secret = crypto.randomBytes(64).toString('hex');
 
-const cors = require('cors');
-
-const axios = require('axios')
-
 const PORT = 3001;
 
 const app = express();
@@ -18,20 +14,14 @@ const app = express();
 const Discogs = require('disconnect').Client;
 const db = new Discogs().database();
 
-const corsOptions = {
-    origin: ['http://samigacon.ide.3wa.io:3000', 'http://localhost:3000'],
-};
 
-// Headers CORS
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    console.log ('Access-Control-Allow-Origin')
-    next();
+// Discogs
+db.getRelease(176126, function(err, data){
+	console.log(data);
 });
 
 
 // Middlewares
-app.use(cors(corsOptions));
 app.use(express.json())
 app.use(express.static('public'))
 app.use('/', routes.router)
