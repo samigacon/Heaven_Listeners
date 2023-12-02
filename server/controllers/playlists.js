ce483f73d8eb486c3f0897a59a23312d7851de84
samigacon <sami.gacon@gmail.com>
2023-12-02 17:38:04 +0000

    Adding Playlist Done

--- server/controllers/playlists.js
+++ server/controllers/playlists.js
@@ -2,12 +2,26 @@ const db = require('../models/database.js')
 const bcrypt = require('bcrypt')
 
 async function addPlaylist(req, res) {
+    try {
+        const { name, description, userId } = req.body;
+        const creationDate = new Date();
 
+        const query = 'INSERT INTO Playlist (Name, Description, Creation_Date, User_ID) VALUES (?, ?, ?, ?)';
+        const values = [name, description, creationDate, userId];
+
+        await db.query(query, values);
+
+        res.status(201).json({ message: 'Playlist Added with Success' });
+    } catch (error) {
+        console.error('Playlist Addition Error :', error);
+        res.status(500).json({ error: 'Playlist Addition Error' });
+    }
 }
 
 async function removePlaylist(req, res) {
 
 }
+
 
 async function renamePlaylist(req, res) {
     try {
@@ -26,4 +40,16 @@ async function renamePlaylist(req, res) {
     }
 }
 
+async function addTrackToPlaylist(req, res) {
+
+}
+
+async function removeTrackFromPlaylist(req, res) {
+
+}
+
+module.exports.addPlaylist = addPlaylist;
+module.exports.removePlaylist = removePlaylist;
 module.exports.renamePlaylist = renamePlaylist;
+module.exports.addTrackToPlaylist = addTrackToPlaylist;
+module.exports.removeTrackFromPlaylist = removeTrackFromPlaylist;
